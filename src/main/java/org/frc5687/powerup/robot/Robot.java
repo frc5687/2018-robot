d26d4ae94f2baadcf699e7d7c2603b0c56f15716
BenBernardCIS <benbernard@cliffislandsoftware.com>
2018-01-15 15:18:51 -0500

    Adding AutoAlign, adding encoder logging.

--- src/main/java/org/frc5687/powerup/robot/Robot.java
+++ src/main/java/org/frc5687/powerup/robot/Robot.java
@@ -79,6 +79,7 @@ public class Robot extends IterativeRobot  {
     }
 
     public void updateDashboard() {
+        driveTrain.updateDashboard();
     }
 
 }
--- src/main/java/org/frc5687/powerup/robot/RobotMap.java
+++ src/main/java/org/frc5687/powerup/robot/RobotMap.java
@@ -7,6 +7,12 @@ public class RobotMap {
         public static final int LEFT_REAR_MOTOR = 1;
         public static final int RIGHT_FRONT_MOTOR = 3;
         public static final int RIGHT_REAR_MOTOR = 4;
+
+        public static final int LEFT_ENCODER_CHANNEL_A = 5;
+        public static final int LEFT_ENCODER_CHANNEL_B = 6;
+        public static final int RIGHT_ENCODER_CHANNEL_A = 7;
+        public static final int RIGHT_ENCODER_CHANNEL_B = 8;
+
     }
 
     public class Climber {
