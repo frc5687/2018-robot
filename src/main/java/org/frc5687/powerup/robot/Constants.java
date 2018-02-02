841d4b877aef0be23c2d5719c0cdae29320b5648
Caleb Marston <caleb.marston@baxter-academy.org>
2018-02-02 17:12:04 -0500

    respecs constants to fit new robots

--- src/main/java/org/frc5687/powerup/robot/Constants.java
+++ src/main/java/org/frc5687/powerup/robot/Constants.java
@@ -89,7 +89,7 @@ public class Constants {
             public static final double INCHES_PER_ROTATION = Math.PI * WHEEL_DIAMETER;
             public static final double SCALAR_RATIO = 8;
             //            public static final double INCHES_PER_PULSE_TONY = INCHES_PER_ROTATION * SCALAR_RATIO / PULSES_PER_ROTATION;
-            public static final double INCHES_PER_PULSE = (1/INCHES_PER_ROTATION)*PULSES_PER_ROTATION;
+            public static final double INCHES_PER_PULSE = INCHES_PER_ROTATION/PULSES_PER_ROTATION;
             // public static final double INCHES_PER_PULSE_TONY = 0.12371134;
             public static final double MAX_PERIOD = 5;
 
