35c88339e481a55a68bc60d82790f3e5fe2194a0
JCharante <git@jcharante.com>
2018-02-03 15:51:12 -0500

    invert intake motors

--- src/main/java/org/frc5687/powerup/robot/Constants.java
+++ src/main/java/org/frc5687/powerup/robot/Constants.java
@@ -10,8 +10,8 @@ public class Constants {
 
     public class Intake {
         public static final double DEADBAND = 0.05;
-        public static final boolean LEFT_MOTORS_INVERTED = false;
-        public static final boolean RIGHT_MOTORS_INVERTED = true;
+        public static final boolean LEFT_MOTORS_INVERTED = true;
+        public static final boolean RIGHT_MOTORS_INVERTED = false;
         public static final double OUTTAKE_SPEED = -0.75;
     }
 
