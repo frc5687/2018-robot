07e831c6e83f25e88308b6b5b7b520a592c69984
Caleb Marston <caleb.marston@baxter-academy.org>
2018-02-07 15:32:14 -0500

    changes scalar to one

--- src/main/java/org/frc5687/powerup/robot/Constants.java
+++ src/main/java/org/frc5687/powerup/robot/Constants.java
@@ -184,7 +184,7 @@ public class Constants {
         public static final double ENCODER_TOP = 340;
 
         public static final double HOLD_SPEED = 0.1;
-        public static final double SPEED_SCALAR = 3.0;
+        public static final double SPEED_SCALAR = 1.0;
 
         public class Pot {
             public static final double TOP = 391.0;
