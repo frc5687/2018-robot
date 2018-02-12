65367e0b3f5c6de728a2712abaff6168f31f138e
Caleb Marston <caleb.marston@baxter-academy.org>
2018-02-12 18:27:44 -0500

    made changes requested in pull request

--- src/main/java/org/frc5687/powerup/robot/Robot.java
+++ src/main/java/org/frc5687/powerup/robot/Robot.java
@@ -59,6 +59,8 @@ public class Robot extends IterativeRobot  {
         intake = new Intake(oi);
         _climber = new Climber(oi);
         _autoChooser = new AutoChooser();
+        SmartDashboard.putBoolean("Competition bot", _isCompetitionBot);
+
 
         try {
             camera = CameraServer.getInstance().startAutomaticCapture(0);
@@ -152,18 +154,21 @@ public class Robot extends IterativeRobot  {
     public void updateDashboard() {
         pdp.updateDashboard();
         _autoChooser.updateDashboard();
-        SmartDashboard.putBoolean("Competition bot", _isCompetitionBot);
+
     }
     public boolean pickConstant(boolean competitionValue, boolean practiceValue){
         return _isCompetitionBot?competitionValue:practiceValue;
     }
+
     public int pickConstant(int competitionValue, int practiceValue){
         return _isCompetitionBot?competitionValue:practiceValue;
     }
 
+
     public double pickConstant(double competitionValue, double practiceValue){
         return _isCompetitionBot?competitionValue:practiceValue;
     }
+
     public boolean isCompetition(){
         return _isCompetitionBot;
     }
