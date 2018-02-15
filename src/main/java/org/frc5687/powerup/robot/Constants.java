7a53101d93b94fffd33f41ba2bad5a5e4c27cef5
Caleb Marston <caleb.marston@baxter-academy.org>
2018-02-15 15:55:51 -0500

    maximizes intake speed

--- src/main/java/org/frc5687/powerup/robot/Constants.java
+++ src/main/java/org/frc5687/powerup/robot/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
         public static final long EJECT_TIME = 250;
 
         public static final double HOLD_SPEED = 0.1;
-        public static final double INTAKE_SPEED = 0.75;
+        public static final double INTAKE_SPEED = 1;
         public static final long SETTLE_TIME = 500;
 
         public class SIDE_IR {
