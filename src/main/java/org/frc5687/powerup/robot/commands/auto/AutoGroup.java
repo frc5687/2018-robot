167a2d4a2d2ad8ebcd51d5bb1aedbb8388952850
JCharante <git@jcharante.com>
2018-02-15 17:50:09 -0500

    move arm up sooner

--- src/main/java/org/frc5687/powerup/robot/commands/auto/AutoGroup.java
+++ src/main/java/org/frc5687/powerup/robot/commands/auto/AutoGroup.java
@@ -139,7 +139,7 @@ public class AutoGroup extends CommandGroup {
                         addSequential(new AutoEject(robot.getIntake()));
                         break;
                     case Constants.AutoChooser.Position.FAR_RIGHT:
-                        addParallel(new PrepIntakeForScale(robot, 220.0, 10000));
+                        addParallel(new PrepIntakeForScale(robot, 35.0, 10000));
                         addSequential(new SixToScale(robot));
                         //addSequential(new AutoAlign(robot.getDriveTrain(), robot.getIMU(), -20, 0.5));
                         addSequential(new AutoEject(robot.getIntake()));
