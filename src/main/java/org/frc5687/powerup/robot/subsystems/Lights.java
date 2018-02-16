d65cf0a5cb2bb37e9234e6b71c2193523aef2bce
Caleb Marston <caleb.marston@baxter-academy.org>
2018-02-16 10:42:11 -0500

    add initial subsystem

---
+++ src/main/java/org/frc5687/powerup/robot/subsystems/Lights.java
@@ -0,0 +1,22 @@
+package org.frc5687.powerup.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Lights extends Subsystem {
+
+        public Lights() {
+        }
+
+        @Override
+        protected void initDefaultCommand() {
+        }
+
+        /**
+         * Checks if cube is detected
+         * @return Whether or not the infrared sensor sees anything
+         */
+
+        public void updateDashboard() {
+        }
+
+}
