824db2732c44ceefb91e08c839803acf9bfe6a89
Caleb Marston <caleb.marston@baxter-academy.org>
2018-02-16 11:30:59 -0500

    adds runLights method which runs the lights based on a two dimensional array

--- src/main/java/org/frc5687/powerup/robot/subsystems/Lights.java
+++ src/main/java/org/frc5687/powerup/robot/subsystems/Lights.java
@@ -15,18 +15,26 @@ public class Lights extends Subsystem {
     private int _status = 0;
     private int _alert = 0;
     private int _turn = 0;
+    private double[][] lightStatus;
 
     public Lights() {
         _left = new Blinkin(RobotMap.Lights.LEFT);
         _right = new Blinkin(RobotMap.Lights.RIGHT);
+        lightStatus = new double[5][5];
     }
 
     @Override
     protected void initDefaultCommand() {
         setDefaultCommand(new RunLights(this));
     }
+    public void runLights(){
+        _left.set(lightStatus[_status][_alert]);
+        _right.set(lightStatus[_status][_alert]);
+    }
 
     public void execute() {
+
+
 
     }
 
