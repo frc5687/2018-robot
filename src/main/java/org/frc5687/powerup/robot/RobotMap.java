abbfc6a93edff51b7daac3d2d4d2819cb7ce9baa
BenBernardCIS <benbernard@cliffislandsoftware.com>
2018-02-18 14:15:37 -0500

    Making name case match programmer and electronics members' preferences.

--- src/main/java/org/frc5687/powerup/robot/RobotMap.java
+++ src/main/java/org/frc5687/powerup/robot/RobotMap.java
@@ -8,108 +8,108 @@ public class RobotMap {
     public static final int IDENTITY_FLAG = 9;
 
     public class DriveTrain {
-        public static final int LEFT_FRONT_MOTOR = PWM.Rio3;
-        public static final int LEFT_REAR_MOTOR = PWM.Rio4;
-        public static final int RIGHT_FRONT_MOTOR = PWM.Rio1;
-        public static final int RIGHT_REAR_MOTOR = PWM.Rio2;
+        public static final int LEFT_FRONT_MOTOR = PWM.RIO3;
+        public static final int LEFT_REAR_MOTOR = PWM.RIO4;
+        public static final int RIGHT_FRONT_MOTOR = PWM.RIO1;
+        public static final int RIGHT_REAR_MOTOR = PWM.RIO2;
 
-        public static final int LEFT_ENCODER_CHANNEL_A = DIO.Rio7;
-        public static final int LEFT_ENCODER_CHANNEL_B = DIO.Rio8;
-        public static final int RIGHT_ENCODER_CHANNEL_A = DIO.Rio5;
-        public static final int RIGHT_ENCODER_CHANNEL_B = DIO.Rio6;
+        public static final int LEFT_ENCODER_CHANNEL_A = DIO.RIO7;
+        public static final int LEFT_ENCODER_CHANNEL_B = DIO.RIO8;
+        public static final int RIGHT_ENCODER_CHANNEL_A = DIO.RIO5;
+        public static final int RIGHT_ENCODER_CHANNEL_B = DIO.RIO6;
 	}
 
     public class Intake {
-        public static final int LEFT_MOTOR = PWM.Rio5 ;
-        public static final int RIGHT_MOTOR = PWM.Rio7 ;
-        public static final int IR_SIDE = ANALOG.Rio0; // Analog
-        public static final int IR_BACK = ANALOG.Rio1; // Analog
-        public static final int SERVO = PWM.Rio0;
+        public static final int LEFT_MOTOR = PWM.RIO5 ;
+        public static final int RIGHT_MOTOR = PWM.RIO7 ;
+        public static final int IR_SIDE = ANALOG.RIO0; // Analog
+        public static final int IR_BACK = ANALOG.RIO1; // Analog
+        public static final int SERVO = PWM.RIO0;
     }
 
     public class Carriage {
-        public static final int MOTOR = PWM.Rio8;
-        public static final int ENCODER_A = DIO.Rio0; // YELLOW
-        public static final int ENCODER_B = DIO.Rio1; // BLUE
+        public static final int MOTOR = PWM.RIO8;
+        public static final int ENCODER_A = DIO.RIO0; // YELLOW
+        public static final int ENCODER_B = DIO.RIO1; // BLUE
 
-        public static final int HALL_EFFECT_TOP = DIO.Rio2; // DIO
-        public static final int HALL_EFFECT_BOTTOM = DIO.Rio3; // DIO
+        public static final int HALL_EFFECT_TOP = DIO.RIO2; // DIO
+        public static final int HALL_EFFECT_BOTTOM = DIO.RIO3; // DIO
     }
 
     public class Arm {
-        public static final int MOTOR = PWM.Rio6;
-        public static final int HALL_EFFECT_STARTING_POSITION = DIO.Rio4;
+        public static final int MOTOR = PWM.RIO6;
+        public static final int HALL_EFFECT_STARTING_POSITION = DIO.RIO4;
         public static final int STARTING_POSITION_LED = 10;
-        public static final int ENCODER_A = DIO.NavX12;
-        public static final int ENCODER_B = DIO.NavX11;
-        public static final int POTENTIOMETER = ANALOG.NavX4;
+        public static final int ENCODER_A = DIO.navX12;
+        public static final int ENCODER_B = DIO.navX11;
+        public static final int POTENTIOMETER = ANALOG.navX4;
     }
 
     public class Climber {
-        public static final int MOTOR = PWM.Rio9;
+        public static final int MOTOR = PWM.RIO9;
     }
 
     public class AutoChooser {
-        public static final int POSITION_SWITCH = ANALOG.Rio2;
-        public static final int MODE_SWITCH = ANALOG.Rio3;
-        public static final int DELAY_SWITCH = ANALOG.NavX5;
+        public static final int POSITION_SWITCH = ANALOG.RIO2;
+        public static final int MODE_SWITCH = ANALOG.RIO3;
+        public static final int DELAY_SWITCH = ANALOG.navX5;
     }
 
     public static class PWM {
-        public static final int Rio0 = 0;
-        public static final int Rio1 = 1;
-        public static final int Rio2 = 2;
-        public static final int Rio3 = 3;
-        public static final int Rio4 = 4;
-        public static final int Rio5 = 5;
-        public static final int Rio6 = 6;
-        public static final int Rio7 = 7;
-        public static final int Rio8 = 8;
-        public static final int Rio9 = 9;
-        public static final int NavX10 = 10;
-        public static final int NavX11 = 11;
-        public static final int NavX12 = 12;
-        public static final int NavX13 = 13;
-        public static final int NavX14 = 14;
-        public static final int NavX15 = 15;
-        public static final int NavX16 = 16;
-        public static final int NavX17 = 17;
-        public static final int NavX18 = 18;
-        public static final int NavX19 = 19;
+        public static final int RIO0 = 0;
+        public static final int RIO1 = 1;
+        public static final int RIO2 = 2;
+        public static final int RIO3 = 3;
+        public static final int RIO4 = 4;
+        public static final int RIO5 = 5;
+        public static final int RIO6 = 6;
+        public static final int RIO7 = 7;
+        public static final int RIO8 = 8;
+        public static final int RIO9 = 9;
+        public static final int navX10 = 10;
+        public static final int navX11 = 11;
+        public static final int navX12 = 12;
+        public static final int navX13 = 13;
+        public static final int navX14 = 14;
+        public static final int navX15 = 15;
+        public static final int navX16 = 16;
+        public static final int navX17 = 17;
+        public static final int navX18 = 18;
+        public static final int navX19 = 19;
     }
 
     public static class DIO {
-        public static final int Rio0 = 0;
-        public static final int Rio1 = 1;
-        public static final int Rio2 = 2;
-        public static final int Rio3 = 3;
-        public static final int Rio4 = 4;
-        public static final int Rio5 = 5;
-        public static final int Rio6 = 6;
-        public static final int Rio7 = 7;
-        public static final int Rio8 = 8;
-        public static final int Rio9 = 9;
-        public static final int NavX10 = 10;
-        public static final int NavX11 = 11;
-        public static final int NavX12 = 12;
-        public static final int NavX13 = 13;
-        public static final int NavX14 = 14;
-        public static final int NavX15 = 15;
-        public static final int NavX16 = 16;
-        public static final int NavX17 = 17;
-        public static final int NavX18 = 18;
-        public static final int NavX19 = 19;
+        public static final int RIO0 = 0;
+        public static final int RIO1 = 1;
+        public static final int RIO2 = 2;
+        public static final int RIO3 = 3;
+        public static final int RIO4 = 4;
+        public static final int RIO5 = 5;
+        public static final int RIO6 = 6;
+        public static final int RIO7 = 7;
+        public static final int RIO8 = 8;
+        public static final int RIO9 = 9;
+        public static final int navX10 = 10;
+        public static final int navX11 = 11;
+        public static final int navX12 = 12;
+        public static final int navX13 = 13;
+        public static final int navX14 = 14;
+        public static final int navX15 = 15;
+        public static final int navX16 = 16;
+        public static final int navX17 = 17;
+        public static final int navX18 = 18;
+        public static final int navX19 = 19;
     }
 
     public static class ANALOG {
-        public static final int Rio0 = 0;
-        public static final int Rio1 = 1;
-        public static final int Rio2 = 2;
-        public static final int Rio3 = 3;
-        public static final int NavX4 = 4;
-        public static final int NavX5 = 5;
-        public static final int NavX6 = 6;
-        public static final int NavX7 = 7;
+        public static final int RIO0 = 0;
+        public static final int RIO1 = 1;
+        public static final int RIO2 = 2;
+        public static final int RIO3 = 3;
+        public static final int navX4 = 4;
+        public static final int navX5 = 5;
+        public static final int navX6 = 6;
+        public static final int navX7 = 7;
     }
 
 }
