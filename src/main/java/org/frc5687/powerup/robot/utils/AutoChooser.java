ce13ad878f4163d0f4685744ab3b9166ab02c26b
BenBernardCIS <benbernard@cliffislandsoftware.com>
2018-02-18 18:48:28 -0500

    Fix #149

--- src/main/java/org/frc5687/powerup/robot/utils/AutoChooser.java
+++ src/main/java/org/frc5687/powerup/robot/utils/AutoChooser.java
@@ -1,6 +1,8 @@
 package org.frc5687.powerup.robot.utils;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import jdk.nashorn.internal.objects.NativeRangeError;
 import org.frc5687.powerup.robot.Constants;
 import org.frc5687.powerup.robot.RobotMap;
 
@@ -37,14 +39,24 @@ public class AutoChooser {
         return delaySwitch.get();
     }
 
+    private static String[] positionLabels = {"Unused","Far Left","Near Left", "Center Left","Center Right", "Near Right","Far Right"};
+    private static String[] modeLabels = {"Stay Put", "Cross The Line", "Switch Face", "Scale", "Unused"};
+    private static long[] delays = {0, 250, 500, 750, 1000, 1250, 1500, 1750, 2000, 2500, 3000, 4000, 5000};
 
     public void updateDashboard(){
+        SmartDashboard.putString("Auto/Position", positionLabels[Math.max(0, positionSwitchValue())]);
+        SmartDashboard.putString("Auto/Mode", modeLabels[Math.max(0, modeSwitchValue())]);
+        SmartDashboard.putString("Auto/Delay", Long.toString(delays[Math.max(0, delaySwitchValue())]) + "ms");
+        SmartDashboard.putString("AutoChooser/Label/Position", positionLabels[Math.max(0, positionSwitchValue())]);
+        SmartDashboard.putString("AutoChooser/Label/Mode", modeLabels[Math.max(0, modeSwitchValue())]);
+        SmartDashboard.putString("AutoChooser/Label/Delay", Long.toString(delays[Math.max(0, delaySwitchValue())]) + "ms");
         SmartDashboard.putNumber("AutoChooser/Raw/Position", positionSwitch.getRaw());
         SmartDashboard.putNumber("AutoChooser/Raw/Mode", modeSwitch.getRaw());
         SmartDashboard.putNumber("AutoChooser/Raw/Delay", delaySwitch.getRaw());
         SmartDashboard.putNumber("AutoChooser/Numeric/Position", positionSwitchValue());
         SmartDashboard.putNumber("AutoChooser/Numeric/Mode", modeSwitchValue());
         SmartDashboard.putNumber("AutoChooser/Numeric/Delay", delaySwitch.get());
+        DriverStation.getInstance().waitForData();
 //        SmartDashboard.putString("AutoChooser/Selection", AutoGroup.getDescription(springSwitch.get(), gearSwitch.get(), hopperSwitch.get()));
   }
 }
