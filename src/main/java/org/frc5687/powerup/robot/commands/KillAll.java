7d9addad8dbed6606f9da4c4802cdf0aaa19b7f6
JCharante <git@jcharante.com>
2018-02-22 13:14:44 -0500

    disable arm and carriage pids in initialize

--- src/main/java/org/frc5687/powerup/robot/commands/KillAll.java
+++ src/main/java/org/frc5687/powerup/robot/commands/KillAll.java
@@ -4,16 +4,27 @@ import edu.wpi.first.wpilibj.command.Command;
 import org.frc5687.powerup.robot.Robot;
 
 public class KillAll extends Command {
+    private boolean _finished;
+    private Robot _robot;
+
     public KillAll(Robot robot) {
         requires(robot.getArm());
         requires(robot.getCarriage());
         requires(robot.getClimber());
         requires(robot.getDriveTrain());
         requires(robot.getIntake());
+        _robot = robot;
+    }
+
+    @Override
+    protected void initialize() {
+        _robot.getArm().disable();
+        _robot.getCarriage().disable();
+        _finished = true;
     }
 
     @Override
     protected boolean isFinished() {
-        return true;
+        return _finished;
     }
 }
