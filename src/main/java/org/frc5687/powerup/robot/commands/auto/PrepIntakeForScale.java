d561588e1beae5023b10907a2f0c08000307475a
JCharante <git@jcharante.com>
2018-02-25 15:56:11 -0500

    modify prep intake for scale to work with is healthy

--- src/main/java/org/frc5687/powerup/robot/commands/auto/PrepIntakeForScale.java
+++ src/main/java/org/frc5687/powerup/robot/commands/auto/PrepIntakeForScale.java
@@ -15,7 +15,11 @@ import org.frc5687.powerup.robot.subsystems.DriveTrain;
 public class PrepIntakeForScale extends CommandGroup {
     public PrepIntakeForScale(Robot robot, double inches, long millis) {
         addSequential(new AutoWaitForDistance(robot.getArm(), robot.getDriveTrain(), inches, millis));
-        addSequential(new MoveCarriageToSetpointPID(robot.getCarriage(), -3));
-        addSequential(new MoveArmToSetpointPID(robot.getArm(), 165));
+        if (robot.getCarriage().isHealthy()) {
+            addSequential(new MoveCarriageToSetpointPID(robot.getCarriage(), -3));
+        }
+        if (robot.getArm().isHealthy()) {
+            addSequential(new MoveArmToSetpointPID(robot.getArm(), 164));
+        }
     }
 }
