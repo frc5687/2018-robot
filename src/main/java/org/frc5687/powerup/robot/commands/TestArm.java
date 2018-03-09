5102b9fe1c0d8d27e3c4fce240d173b856473516
Caleb Marston <caleb.marston@baxter-academy.org>
2018-03-09 11:33:15 -0500

    add move arm to setpoint to test arm

--- src/main/java/org/frc5687/powerup/robot/commands/TestArm.java
+++ src/main/java/org/frc5687/powerup/robot/commands/TestArm.java
@@ -2,13 +2,17 @@ package org.frc5687.powerup.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.frc5687.powerup.robot.Constants;
 import org.frc5687.powerup.robot.Robot;
 import org.frc5687.powerup.robot.subsystems.Arm;
 import org.frc5687.powerup.robot.subsystems.Carriage;
 
 public class TestArm extends CommandGroup{
     public TestArm(Robot robot){
-        addSequential(new ArmMotorTest(robot.getArm(), robot.getCarriage()), 0.375);
+        addSequential(new ArmMotorTest(robot.getArm(), robot.getCarriage()));
+        addSequential(new MoveArmToSetpointPID(robot.getArm(), Constants.Arm.Encoder.ENCODER_START));
+        addSequential(new MoveArmToSetpointPID(robot.getArm(), Constants.Arm.Encoder.ENCODER_TOP));
+
     }
 
 
