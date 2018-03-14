5f3046dc67bb1f2f5dbb776de8436ea09250336b
Caleb Marston <caleb.marston@baxter-academy.org>
2018-03-14 13:28:28 -0400

    add zero carriage to test

--- src/main/java/org/frc5687/powerup/robot/commands/TestCarriage.java
+++ src/main/java/org/frc5687/powerup/robot/commands/TestCarriage.java
@@ -1,6 +1,13 @@
 package org.frc5687.powerup.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.frc5687.powerup.robot.Constants;
+import org.frc5687.powerup.robot.Robot;
+import org.frc5687.powerup.robot.commands.auto.AutoZeroCarriage;
 
 public class TestCarriage extends CommandGroup {
+    public TestCarriage(Robot robot){
+        addSequential(new TestCarriageMotor(robot.getCarriage()));
+        addSequential(new AutoZeroCarriage(robot.getCarriage()));
+    }
 }
