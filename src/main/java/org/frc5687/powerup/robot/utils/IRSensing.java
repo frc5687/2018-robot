abe0a82ec296f1dd904b9571e46be5c91b1a9128
jayden-PC\jayden <jayden.butts@baxter-academy.org>
2018-03-15 20:53:53 -0400

    Convert cm to inches

--- src/main/java/org/frc5687/powerup/robot/utils/IRSensing.java
+++ src/main/java/org/frc5687/powerup/robot/utils/IRSensing.java
@@ -17,21 +17,21 @@ public class IRSensing extends AnalogInput {
 
     public class sensorShort {
         public double getDistance() {
-            return Constants.IRPID.TRANSFORM_COEFFICIENT_SHORT * Math.pow(getRaw(), Constants.IRPID.TRANSFORM_POWER_SHORT);
+            return Constants.IRPID.TRANSFORM_COEFFICIENT_SHORT * Math.pow(getRaw(), Constants.IRPID.TRANSFORM_POWER_SHORT) / 2.54;
         }
     }
 
 
     public class sensorMedium {
         public double getDistance() {
-            return Constants.IRPID.TRANSFORM_COEFFICIENT_MEDIUM * Math.pow(getRaw(), Constants.IRPID.TRANSFORM_POWER_MEDIUM);
+            return Constants.IRPID.TRANSFORM_COEFFICIENT_MEDIUM * Math.pow(getRaw(), Constants.IRPID.TRANSFORM_POWER_MEDIUM) / 2.54;
         }
     }
 
 
     public class sensorLong {
         public double getDistance() {
-            return Constants.IRPID.TRANSFORM_COEFFICIENT_LONG * Math.pow(getRaw(), Constants.IRPID.TRANSFORM_POWER_LONG);
+            return Constants.IRPID.TRANSFORM_COEFFICIENT_LONG * Math.pow(getRaw(), Constants.IRPID.TRANSFORM_POWER_LONG) / 2.54;
 
         }
     }
