1c02ebd785f0b2bd70cff71a01132194dea87681
JCharante <git@jcharante.com>
2018-03-17 11:10:20 -0400

    lower past 90 deg hold speed

--- src/main/java/org/frc5687/powerup/robot/Constants.java
+++ src/main/java/org/frc5687/powerup/robot/Constants.java
@@ -403,9 +403,9 @@ public class Constants {
             public static final double PAST_160_NO_CUBE_GRETA = 0.25;
 
             public static final double PAST_90_CUBE_PROTO = 0.0;
-            public static final double PAST_90_CUBE_GRETA = 0.25;
+            public static final double PAST_90_CUBE_GRETA = 0.1;
             public static final double PAST_90_NO_CUBE_PROTO = 0.0;
-            public static final double PAST_90_NO_CUBE_GRETA = 0.25;
+            public static final double PAST_90_NO_CUBE_GRETA = 0.1;
 
             public static final double PAST_40_CUBE_PROTO = 0.0;
             public static final double PAST_40_CUBE_GRETA = 0.1;
