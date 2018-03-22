8a98eecf56f8cbdb7c3e72c351ea3c83b2d4f24b
JCharante <git@jcharante.com>
2018-03-21 21:32:41 -0400

    fix logic bug with carriage hold speed calculation and update hold speeds

--- src/main/java/org/frc5687/powerup/robot/Constants.java
+++ src/main/java/org/frc5687/powerup/robot/Constants.java
@@ -423,14 +423,14 @@ public class Constants {
             public static final double PAST_90_NO_CUBE_PROTO = 0.0;
             public static final double PAST_90_NO_CUBE_GRETA = 0.1;
 
-            public static final double PAST_40_CUBE_PROTO = -0.1;
+            public static final double PAST_40_CUBE_PROTO = -0.2;
             public static final double PAST_40_CUBE_GRETA = -0.1;
-            public static final double PAST_40_NO_CUBE_PROTO = -0.1;
+            public static final double PAST_40_NO_CUBE_PROTO = -0.2;
             public static final double PAST_40_NO_CUBE_GRETA = -0.1;
 
-            public static final double PAST_BOTTOM_CUBE_PROTO = 0.0;
+            public static final double PAST_BOTTOM_CUBE_PROTO = -0.1;
             public static final double PAST_BOTTOM_CUBE_GRETA = -0.20;
-            public static final double PAST_BOTTOM_NO_CUBE_PROTO = 0.0;
+            public static final double PAST_BOTTOM_NO_CUBE_PROTO = -0.1;
             public static final double PAST_BOTTOM_NO_CUBE_GRETA = -0.20;
         }
     }
