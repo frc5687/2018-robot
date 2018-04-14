882c2f6bc55a0861e61674229befd2e8b9b8301d
JCharante <git@jcharante.com>
2018-04-14 06:49:20 -0400

    update to use new method of aborting auton

--- src/main/java/org/frc5687/powerup/robot/commands/auto/AbortIfNoCubeDetected.java
+++ src/main/java/org/frc5687/powerup/robot/commands/auto/AbortIfNoCubeDetected.java
@@ -2,15 +2,17 @@ package org.frc5687.powerup.robot.commands.auto;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
+import org.frc5687.powerup.robot.Robot;
 import org.frc5687.powerup.robot.subsystems.Intake;
 
 public class AbortIfNoCubeDetected extends Command {
+    private Robot _robot;
     private Intake _intake;
     private boolean _isFinished;
 
-    public AbortIfNoCubeDetected(Intake intake) {
-        _intake = intake;
+    public AbortIfNoCubeDetected(Robot robot) {
+        _robot = robot;
+        _intake = robot.getIntake();
     }
 
     @Override
@@ -18,8 +20,8 @@ public class AbortIfNoCubeDetected extends Command {
         DriverStation.reportError("AbortIfNoCubeDetected initialized", false);
         _isFinished = true;
         if (!_intake.cubeIsSecured()) {
-            DriverStation.reportError("AbortIfNoCubeDetected no cube detected.. running Scheduler.getInstance().removeAll()", false);
-            Scheduler.getInstance().removeAll();
+            DriverStation.reportError("AbortIfNoCubeDetected no cube detected.. calling robot.requestAbortAuton()", false);
+            _robot.requestAbortAuton();
         }
     }
 
