f9177993c1f1818cf171e21197afc51ec6ccca9f
Caleb Marston <caleb.marston@baxter-academy.org>
2018-04-16 20:00:12 -0400

    add full self test

---
+++ src/main/java/org/frc5687/powerup/robot/commands/testing/FullSelfTest.java
@@ -0,0 +1,9 @@
+package org.frc5687.powerup.robot.command.testing;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class FullSelfTest extends CommandGroup {
+    public FullSelfTest() {
+
+    }
+}
