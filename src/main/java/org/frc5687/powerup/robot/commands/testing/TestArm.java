2d2cde2fe278f348600abce0f85d1f1c7cf8121c
Caleb Marston <caleb.marston@baxter-academy.org>
2018-04-19 19:41:05 -0400

    merge the arm self test in

--- src/main/java/org/frc5687/powerup/robot/commands/testing/TestArm.java
+++ src/main/java/org/frc5687/powerup/robot/commands/testing/TestArm.java
@@ -1,4 +1,4 @@
-package org.frc5687.powerup.robot.commands;
+package org.frc5687.powerup.robot.commands.testing;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -6,6 +6,8 @@ import org.frc5687.powerup.robot.Constants;
 import org.frc5687.powerup.robot.Robot;
 import org.frc5687.powerup.robot.subsystems.Arm;
 import org.frc5687.powerup.robot.subsystems.Carriage;
+import org.frc5687.powerup.robot.commands.testing.ArmMotorTest;
+import org.frc5687.powerup.robot.commands.MoveArmToSetpointPID;
 
 public class TestArm extends CommandGroup{
     public TestArm(Robot robot){
