28abe6542e88ab2e8c63c986977e621e418e7a65
JCharante <git@jcharante.com>
2018-04-25 20:22:42 -0400

    update max pitch and roll to 10 from 7, since 8 was reached on a flat surface.

--- src/main/java/org/frc5687/powerup/robot/Constants.java
+++ src/main/java/org/frc5687/powerup/robot/Constants.java
@@ -165,8 +165,8 @@ public class Constants {
         public static final double MIN_IMU_ANGLE = -180;
         public static final double MAX_IMU_ANGLE = 180;
 
-        public static final double MAX_PITCH = 7.0;
-        public static final double MAX_ROLL = 7.0;
+        public static final double MAX_PITCH = 10.0;
+        public static final double MAX_ROLL = 10.0;
 
         public class Align {
 
